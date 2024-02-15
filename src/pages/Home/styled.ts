081560c462765f0bcb82e2a594948a4d4dc3787a
victor_ferreira <vitorsantosferreira10@gmail.com>
2024-02-14 23:47:37 -0300

    feat: finished page home, config inputs and components
    Signed-off-by: victor_ferreira <vitorsantosferreira10@gmail.com>

--- src/pages/Home/styled.ts
+++ src/pages/Home/styled.ts
@@ -28,11 +28,31 @@ export const FromContainer = styled.div`
 `
 const BaseInput = styled.input`
   background: transparent;
+  border: 0;
+  height: 2.5rem;
+  border-bottom: solid 2px ${(props) => props.theme['gray-500']};
+  font-weight: bold;
+  font-size: 1.125rem;
+  padding: 0 0.5rem;
+  color: ${(props) => props.theme['gray-100']};
+
+  &:focus {
+    box-shadow: none;
+    border-color: ${(props) => props.theme['green-500']};
+  }
+
+  &:placeholder {
+    color: ${(props) => props.theme['gray-500']};
+  }
 `
 
-export const TaskInput = styled(BaseInput)``
+export const TaskInput = styled(BaseInput)`
+  flex: 1;
+`
 
-export const MinutesAmountInput = styled(BaseInput)``
+export const MinutesAmountInput = styled(BaseInput)`
+  width: 4rem;
+`
 
 export const CountdownContainer = styled.div`
   font-family: 'Roboto Mono', monospace;
