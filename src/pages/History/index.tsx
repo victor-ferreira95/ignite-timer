b58712e18a9a831863ce91444eb37645617b15f9
victor_ferreira <vitorsantosferreira10@gmail.com>
2024-02-27 22:48:08 -0300

    styled: custom component status

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from './styles'
+import { HistoryContainer, HistoryList, Status } from './styles'
 
 export function History() {
   return (
@@ -19,25 +19,33 @@ export function History() {
               <td>ZapConeta</td>
               <td>20 minutos</td>
               <td>ha 2 segundos</td>
-              <td>Pendente</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
             </tr>
             <tr>
               <td>ZapConeta</td>
               <td>20 minutos</td>
               <td>ha 2 segundos</td>
-              <td>Pendente</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
             </tr>
             <tr>
               <td>ZapConeta</td>
               <td>20 minutos</td>
               <td>ha 2 segundos</td>
-              <td>Pendente</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
             </tr>
             <tr>
               <td>ZapConeta</td>
               <td>20 minutos</td>
               <td>ha 2 segundos</td>
-              <td>Pendente</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
             </tr>
           </tbody>
         </table>
